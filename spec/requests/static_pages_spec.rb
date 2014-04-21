2815c5112993f6afaf2cf1b5ec7eb21e75059088
Koen Klasing <koen@bureaublauwgeel.nl>
2014-04-21 21:43:13 +0200

    Added some static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,39 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+
+  describe "Home page" do
+    it "should have the content 'Recepten App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Recepten App')
+    end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Recepten App | Home")
     end
   end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Recepten App | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'Over de Recepten App'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('Over de Recepten App')
+    end
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Recepten App | About")
+    end
+  end
+
+
 end
